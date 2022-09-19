5010299dbae666eafd3d435be5638cbaecc26fa4
jayaprasad356 <jayaprasad356@gmail.com>
2022-09-19 23:39:47 +0530

    all data list

---
+++ api/alldatalist.php
@@ -0,0 +1,46 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+
+include_once('../includes/crud.php');
+$db = new Database();
+$db->connect();
+
+
+$sql = "SELECT * FROM `panchangam`";
+$db->sql($sql);
+$res = $db->getResult();
+$rows = array();
+foreach ($res as $row) {
+    $temp['id'] = $row['id'];
+    $temp['date'] = $row['date'];
+    $temp['sunrise'] = $row['sunrise'];
+    $temp['sunset'] = $row['sunset'];
+    $temp['moonrise'] = $row['moonrise'];
+    $temp['moonset'] = $row['moonset'];
+    $temp['info'] = $row['info'];
+    $rows[] = $temp;
+}
+$response['success'] = true;
+$response['message'] = "Panchangam List Successfullty";
+$response['panchangam_list'] = $rows;
+$sql = "SELECT * FROM `panchangam_variant`";
+$db->sql($sql);
+$res = $db->getResult();
+$rows = array();
+foreach ($res as $row) {
+    $temp['id'] = $row['id'];
+    $temp['panchangam_id'] = $row['panchangam_id'];
+    $temp['title'] = $row['title'];
+    $temp['description'] = $row['description'];
+    $rows[] = $temp;
+}
+$response['panchangam_tab_list'] = $rows;
+print_r(json_encode($response));
+?>
