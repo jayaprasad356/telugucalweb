87bef368a95685b14f7428482593e386b285e573
jayaprasad356 <jayaprasad356@gmail.com>
2022-10-16 09:58:16 +0530

    api updated

---
+++ api/images-category.php
@@ -0,0 +1,38 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+date_default_timezone_set('Asia/Kolkata');
+
+include_once('../includes/crud.php');
+$db = new Database();
+$db->connect();
+$sql = "SELECT * FROM `image_category`";
+$db->sql($sql);
+$res = $db->getResult();
+$num = $db->numRows($res);
+if($num>=1){
+    $rows = array();
+    $temp = array();
+    foreach ($res as $row) {
+        $temp['id'] = $row['id'];
+        $temp['name'] = $row['name'];
+        $temp['image'] = DOMAIN_URL . $row['image'];
+        $rows[] = $temp;
+    }
+    $response['success'] = true;
+    $response['message'] = "Image Category List Successfullty";
+    $response['data'] = $rows;
+    print_r(json_encode($response));
+
+}
+else{
+    $response['success'] = false;
+    $response['message'] = "Not Found";
+    print_r(json_encode($response));
+}
+?>
