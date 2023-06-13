1a12385b8c08d045d545b795d845feea8f1647a2
jayaprasad356 <jayaprasad356@gmail.com>
2023-06-13 12:58:37 +0530

    panchangam_list

--- api/panchangam-list.php
+++ api/panchangam-list.php
@@ -11,14 +11,68 @@ date_default_timezone_set('Asia/Kolkata');
 include_once('../includes/crud.php');
 $db = new Database();
 $db->connect();
-if (empty($_POST['day'])) {
-    $response['success'] = false;
-    $response['message'] = "Day is Empty";
-    print_r(json_encode($response));
-    return false;
+$day = (isset($_POST['day']) && !empty($_POST['day'])) ? $db->escapeString($_POST['day']) : "";
+if (isset($_POST['all']) && $_POST['all'] == 1) {
+    $sql = "SELECT * FROM `panchangam`";
+    $db->sql($sql);
+    $res = $db->getResult();
+    $num = $db->numRows($res);
+    if($num>=1){
+        foreach ($res as $row) {
+            $temp['id'] = $row['id'];
+            $temp['date'] = DateTime::createFromFormat('Y-m-d', $row['date'])->format('d-m-Y');
+            $temp['text1']= $row['text1'];
+            $temp['text2']= $row['text2'];
+            $temp['text3']= $row['text3'];
+            $temp['text4']= $row['text4'];
+            $temp['text5']= $row['text5'];
+            $temp['text6']= $row['text6'];
+            $temp['sunrise'] =$row['sunrise'];
+            $temp['sunset'] = $row['sunset'];
+            $temp['moonrise'] = $row['moonrise'];
+            $temp['moonset'] = $row['moonset'];
+            $temp['festivals'] = $row['festivals'];
+            $temp['thidhi'] = $row['thidhi'];
+            $temp['nakshatram'] = $row['nakshatram'];
+            $temp['yogam'] = $row['yogam'];
+            $temp['karanam'] = $row['karanam'];
+            $temp['abhijith_muhurtham'] = $row['abhijith_muhurtham'];
+            $temp['bhrama_muhurtham'] = $row['bhrama_muhurtham'];
+            $temp['amrutha_kalam'] = $row['amrutha_kalam'];
+            $temp['rahukalam'] = $row['rahukalam'];
+            $temp['yamakandam'] = $row['yamakandam'];
+            $temp['dhurmuhurtham'] = $row['dhurmuhurtham'];
+            $temp['varjyam'] = $row['varjyam'];
+            $temp['gulika'] =$row['gulika'];
+            $temp['hc1']= $row['hc1'];
+            $temp['hc2']= $row['hc2'];
+            $temp['hc3']= $row['hc3'];
+            $temp['hc4']= $row['hc4'];
+            $temp['hc5']= $row['hc5'];
+            $temp['hc6']= $row['hc6'];
+            $temp['hc7']= $row['hc7'];
+            $temp['hc8']= $row['hc8'];
+            $temp['hc9']= $row['hc9'];
+            $temp['hc10']= $row['hc10'];
+            $temp['hc11']= $row['hc11'];
+            $temp['hc12']= $row['hc12'];
+            $rows[] = $temp;
+        }
+     
+        $response['success'] = true;
+        $response['message'] = "Panchangam Listed Successfully";
+        $response['data'] = $rows;
+        print_r(json_encode($response));
+    
+    }
+    else{
+        $response['success'] = false;
+        $response['message'] = "Data Not Found";
+        print_r(json_encode($response));
+    }
+
 }
-$day = $db->escapeString($_POST['day']);
-if (isset($_POST['gowri']) && $_POST['gowri'] == 1) {
+if (isset($_POST['day']) && isset($_POST['gowri']) && $_POST['gowri'] == 1) {
     $sql = "SELECT * FROM `gowri` WHERE day = '$day'";
     $db->sql($sql);
     $res = $db->getResult();
@@ -47,7 +101,7 @@ if (isset($_POST['gowri']) && $_POST['gowri'] == 1) {
     }
 
 }
-if (isset($_POST['hora_chakram']) && $_POST['hora_chakram'] == 1) {
+if (isset($_POST['day']) && isset($_POST['hora_chakram']) && $_POST['hora_chakram'] == 1) {
     $sql = "SELECT * FROM `hora_chakram` WHERE day = '$day'";
     $db->sql($sql);
     $res = $db->getResult();
@@ -76,7 +130,7 @@ if (isset($_POST['hora_chakram']) && $_POST['hora_chakram'] == 1) {
     }
 
 }
-if (isset($_POST['bhargava_panchangam']) && $_POST['bhargava_panchangam'] == 1) {
+if (isset($_POST['day']) && isset($_POST['bhargava_panchangam']) && $_POST['bhargava_panchangam'] == 1) {
     $sql = "SELECT * FROM `bhargava_panchangam` WHERE day = '$day'";
     $db->sql($sql);
     $res = $db->getResult();
@@ -104,7 +158,7 @@ if (isset($_POST['bhargava_panchangam']) && $_POST['bhargava_panchangam'] == 1) 
     }
 
 }
-if (isset($_POST['thidhi']) && $_POST['thidhi'] == 1) {
+if (isset($_POST['day']) && isset($_POST['thidhi']) && $_POST['thidhi'] == 1) {
     $year = $db->escapeString($_POST['year']);
     $month = $db->escapeString($_POST['month']);
     $sql = "SELECT * FROM `thidhi` t,`thidhi_variant` tv WHERE tv.thidhi_id = t.id AND t.month = '$month' AND t.year = '$year'";
@@ -135,7 +189,7 @@ if (isset($_POST['thidhi']) && $_POST['thidhi'] == 1) {
     }
 
 }
-if (isset($_POST['rahukalam']) && $_POST['rahukalam'] == 1) {
+if (isset($_POST['day']) && isset($_POST['rahukalam']) && $_POST['rahukalam'] == 1) {
     $year = $db->escapeString($_POST['year']);
     $day = $db->escapeString($_POST['day']);
     $sql = "SELECT * FROM `rahukalams` WHERE day = '$day' AND year = '$year'";
@@ -166,7 +220,7 @@ if (isset($_POST['rahukalam']) && $_POST['rahukalam'] == 1) {
     }
 
 }
-if (isset($_POST['karanam']) && $_POST['karanam'] == 1) {
+if (isset($_POST['day']) && isset($_POST['karanam']) && $_POST['karanam'] == 1) {
     $sql = "SELECT * FROM `karanam`";
     $db->sql($sql);
     $res = $db->getResult();
@@ -185,7 +239,7 @@ if (isset($_POST['karanam']) && $_POST['karanam'] == 1) {
     }
 
 }
-if (isset($_POST['yogam']) && $_POST['yogam'] == 1) {
+if (isset($_POST['day']) && isset($_POST['yogam']) && $_POST['yogam'] == 1) {
     $sql = "SELECT * FROM `yogam`";
     $db->sql($sql);
     $res = $db->getResult();
