6ad4e0cae1345b33c375a0b57aa4ec2abae6a940
AKraja777 <ashokkumar7hitter@gmail.com>
2023-07-07 12:01:41 +0530

    edit-samkrutham_images-form.php

--- public/edit-samkrutham_images-form.php
+++ public/edit-samkrutham_images-form.php
@@ -3,8 +3,6 @@ include_once('includes/functions.php');
 $function = new functions;
 include_once('includes/custom-functions.php');
 $fn = new custom_functions;
-?>
-<?php
 
 if (isset($_GET['id'])) {
     $ID = $db->escapeString($_GET['id']);
@@ -16,52 +14,46 @@ if (isset($_GET['id'])) {
 
 if (isset($_POST['btnEdit'])) {
 
-	$title = $db->escapeString(($_POST['title']));
-	$error = array();
+    $title = $db->escapeString(($_POST['title']));
+    $error = array();
 
-   
-   
-   if (!empty($title))
-    {
-		if ($_FILES['image']['size'] != 0 && $_FILES['image']['error'] == 0 && !empty($_FILES['image'])) {
-			//image isn't empty and update the image
-			$old_image = $db->escapeString($_POST['old_image']);
-			$extension = pathinfo($_FILES["image"]["name"])['extension'];
-	
-			$result = $fn->validate_image($_FILES["image"]);
-			$target_path = 'upload/images/';
-			
-			$filename = microtime(true) . '.' . strtolower($extension);
-			$full_path = $target_path . "" . $filename;
-			if (!move_uploaded_file($_FILES["image"]["tmp_name"], $full_path)) {
-				echo '<p class="alert alert-danger">Can not upload image.</p>';
-				return false;
-				exit();
-			}
-			if (!empty($old_image)) {
-				unlink($old_image);
-			}
-			$upload_image = 'upload/images/' . $filename;
-			$sql = "UPDATE telugu_samkrutham SET `image`='" . $upload_image . "' WHERE `id`=" . $ID;
-			$db->sql($sql);
-		}
-		$update_result = $db->getResult();
-		if (!empty($update_result)) {
-			$update_result = 0;
-		} else {
-			$update_result = 1;
+    if (!empty($title)) {
+        if ($_FILES['image']['size'] != 0 && $_FILES['image']['error'] == 0 && !empty($_FILES['image'])) {
+            //image isn't empty and update the image
+            $old_image = $db->escapeString($_POST['old_image']);
+            $extension = pathinfo($_FILES["image"]["name"])['extension'];
 
-		}
-			// check update result
-			if ($update_result == 1) {
-				
-			$error['update_images'] = " <section class='content-header'><span class='label label-success'>Telugu Samkrutham Image updated Successfully</span></section>";
-			} else {
-				$error['update_images'] = " <span class='label label-danger'>Failed to update</span>";
-			}
-		}
-	} 
+            $result = $fn->validate_image($_FILES["image"]);
+            $target_path = 'upload/images/';
 
+            $filename = microtime(true) . '.' . strtolower($extension);
+            $full_path = $target_path . "" . $filename;
+            if (!move_uploaded_file($_FILES["image"]["tmp_name"], $full_path)) {
+                echo '<p class="alert alert-danger">Can not upload image.</p>';
+                return false;
+                exit();
+            }
+            if (!empty($old_image)) {
+                unlink($old_image);
+            }
+            $upload_image = 'upload/images/' . $filename;
+            $sql = "UPDATE telugu_samkrutham SET `image`='" . $upload_image . "' WHERE `id`=" . $ID;
+            $db->sql($sql);
+        }
+        $update_result = $db->getResult();
+        if (!empty($update_result)) {
+            $update_result = 0;
+        } else {
+            $update_result = 1;
+        }
+        // check update result
+        if ($update_result == 1) {
+            $error['update_images'] = " <section class='content-header'><span class='label label-success'>Telugu Samkrutham Image updated Successfully</span></section>";
+        } else {
+            $error['update_images'] = " <span class='label label-danger'>Failed to update</span>";
+        }
+    }
+}
 
 // create array variable to store previous data
 $data = array();
@@ -71,84 +63,78 @@ $db->sql($sql_query);
 $res = $db->getResult();
 
 if (isset($_POST['btnCancel'])) { ?>
-	<script>
-		window.location.href = "samkrutham_images.php";
-	</script>
+    <script>
+        window.location.href = "samkrutham_images.php";
+    </script>
 <?php } ?>
 <section class="content-header">
-	<h1>
-		Edit Telugu Samkrutham Image<small><a href='samkrutham_images.php'><i class='fa fa-angle-double-left'></i>&nbsp;&nbsp;&nbsp;Back to Images</a></small></h1>
-	<small><?php echo isset($error['update_images']) ? $error['update_images'] : ''; ?></small>
-	<ol class="breadcrumb">
-		<li><a href="home.php"><i class="fa fa-home"></i> Home</a></li>
-	</ol>
+    <h1>
+        Edit Telugu Samkrutham Image<small><a href='samkrutham_images.php'><i class='fa fa-angle-double-left'></i>&nbsp;&nbsp;&nbsp;Back to Images</a></small></h1>
+    <small><?php echo isset($error['update_images']) ? $error['update_images'] : ''; ?></small>
+    <ol class="breadcrumb">
+        <li><a href="home.php"><i class="fa fa-home"></i> Home</a></li>
+    </ol>
 </section>
 <section class="content">
-	<!-- Main row -->
-
-	<div class="row">
-		<div class="col-md-6">
-		
-			<!-- general form elements -->
-			<div class="box box-primary">
-				<div class="box-header with-border">
-				</div>
-				<div class="box-header">
+    <!-- Main row -->
+    <div class="row">
+        <div class="col-md-6">
+            <!-- general form elements -->
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                </div>
+                <div class="box-header">
                     <?php echo isset($error['cancelable']) ? '<span class="label label-danger">Till status is required.</span>' : ''; ?>
                 </div>
-				
-				<!-- /.box-header -->
-				<!-- form start -->
-				<form id="edit_grahalu_form" method="post" enctype="multipart/form-data">
-					<div class="box-body">
-					<input type="hidden" id="old_image" name="old_image"  value="<?= $res[0]['image']; ?>">
-						   <div class="row">
-							    <div class="form-group">
-									<div class='col-md-12'>
-										<label for="exampleInputEmail1">Title</label> <i class="text-danger asterik">*</i>
-										<input type="text" class="form-control" name="title" value="<?php echo $res[0]['title']; ?>" disabled>
-									</div>
-								</div>
-						   </div>
-						   <br>
-						   <div class="row">
-								<div class="fom-group">
-									<div class="col-md-6">
-									<label for="exampleInputFile">Image</label><i class="text-danger asterik">*</i>
-											
-											<input type="file" accept="image/png,  image/jpeg" onchange="readURL(this);"  name="image" id="image">
-											<p class="help-block"><img id="blah" src="<?php echo $res[0]['image']; ?>" style="height:100px;max-width:100%" /></p>
-									</div>
-								</div>
-                            </div>   
-						
-						</div>
-						<!-- /.box-body -->
-                       
-					<div class="box-footer">
-						<button type="submit" class="btn btn-primary" name="btnEdit">Update</button>					
-					</div>
-				</form>
-			</div><!-- /.box -->
-		</div>
-	</div>
+                <!-- /.box-header -->
+                <!-- form start -->
+                <form id="edit_grahalu_form" method="post" enctype="multipart/form-data">
+                    <div class="box-body">
+                        <input type="hidden" id="old_image" name="old_image" value="<?= $res[0]['image']; ?>">
+                        <div class="row">
+                            <div class="form-group">
+                                <div class='col-md-12'>
+                                    <label for="exampleInputEmail1">Title</label> <i class="text-danger asterik">*</i>
+                                    <input type="text" class="form-control" name="title" value="<?php echo $res[0]['title']; ?>" disabled>
+                                </div>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="fom-group">
+                                <div class="col-md-6">
+                                    <label for="exampleInputFile">Image</label><i class="text-danger asterik">*</i>
+                                    <input type="file" accept="image/png, image/jpeg" onchange="readURL(this);" name="image" id="image">
+                                    <p class="help-block"><img id="blah" src="<?php echo $res[0]['image']; ?>" style="height:100px;max-width:100%" /></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.box-body -->
+                    <div class="box-footer">
+                        <button type="submit" class="btn btn-primary" name="btnEdit">Update</button>
+                    </div>
+                </form>
+            </div><!-- /.box -->
+        </div>
+    </div>
 </section>
 
 <div class="separator"> </div>
 <script>
     function readURL(input) {
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
 
-                reader.onload = function (e) {
-                    $('#blah')
-                        .attr('src', e.target.result)
-                        .width(400)
-                        .height(250);
-                };
+            reader.onload = function (e) {
+                $('#blah')
+                    .attr('src', e.target.result)
+                    .width(400)
+                    .height(250);
+            };
 
-                reader.readAsDataURL(input.files[0]);
-            }
+            reader.readAsDataURL(input.files[0]);
         }
+    }
 </script>
 <?php $db->disconnect(); ?>
