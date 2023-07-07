6675b4176e682e6d32a2f6ec93e5cad06d428a5c
AKraja777 <ashokkumar7hitter@gmail.com>
2023-07-07 18:41:45 +0530

    crud.php

--- includes/crud.php
+++ includes/crud.php
@@ -15,10 +15,10 @@ class Database
      */
 //define('DOMAIN_URL', 'https://telugucalweb.graymatterworks.com/'); 
     //Live
-    private $db_host = "localhost";  
-   private $db_user = "u743445510_telugucalweb";  
-    private $db_pass = "Tcal@2022";  
-    private $db_name = "u743445510_telugucalweb";    
+    //private $db_host = "localhost";  
+   //private $db_user = "u743445510_telugucalweb";  
+   // private $db_pass = "Tcal@2022";  
+   // private $db_name = "u743445510_telugucalweb";    
 
     //Test
     // private $db_host = "localhost";  // Change as required
