f1865f13ebb0c2a834bb7e6790c31038d5f0f2e6
AKraja777 <ashokkumar7hitter@gmail.com>
2023-07-18 23:09:30 +0530

    edit-grahalu_tab-form.php

--- public/edit-grahalu_tab-form.php
+++ public/edit-grahalu_tab-form.php
@@ -170,13 +170,13 @@ if (isset($_POST['btnCancel'])) { ?>
 									    <div class="col-md-4">
 											<div class="form-group packate_div">
 												<label for="exampleInputEmail1">sub_title</label> <i class="text-danger asterik">*</i>
-												<input type="text" class="form-control" name="sub_title[]" value="<?php echo $row['sub_title'] ?>" required/>
+												<input type="text" class="form-control" name="sub_title[]" value="<?php echo $row['sub_title'] ?>" />
 											</div>
 										</div>
 										<div class="col-md-4">
 											<div class="form-group packate_div">
 												<label for="exampleInputEmail1"> sub_description</label> <i class="text-danger asterik">*</i>
-												<textarea type="text" rows="2" class="form-control" name="sub_description[]" required><?php echo $row['sub_description'] ?></textarea>
+												<textarea type="text" rows="2" class="form-control" name="sub_description[]"><?php echo $row['sub_description'] ?></textarea>
 											</div>
 										</div>
 
