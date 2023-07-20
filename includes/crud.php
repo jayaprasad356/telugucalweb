c58712d0fe421f1eaafa1d0e3baad3f6956dde85
AKraja777 <ashokkumar7hitter@gmail.com>
2023-07-20 18:25:38 +0530

    crud.php

--- includes/crud.php
+++ includes/crud.php
@@ -22,9 +22,9 @@ class Database
 
     //Test
      private $db_host = "localhost";  // Change as required
-     private $db_user = "root";  // Change as required
-     private $db_pass = "";  // Change as required
-     private $db_name = "telugu_calendar";    // Change as required
+     private $db_user = "u762811021_tc";  // Change as required
+     private $db_pass = "Tcal@2022";  // Change as required
+     private $db_name = "u762811021_tc";    // Change as required
 
     /*
      * Extra variables that are required by other function such as boolean con variable
