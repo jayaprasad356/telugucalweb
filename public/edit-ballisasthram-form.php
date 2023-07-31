b78b1faf747cfcfd38fab7aa5879808326fa1467
AKraja777 <ashokkumar7hitter@gmail.com>
2023-07-31 17:58:26 +0530

    edit-ballisasthram-form.php

--- public/edit-ballisasthram-form.php
+++ public/edit-ballisasthram-form.php
@@ -1,4 +1,3 @@
-
 <?php
 include_once('includes/functions.php');
 date_default_timezone_set('Asia/Kolkata');
@@ -9,9 +8,8 @@ $fn = new custom_functions;
 if (isset($_GET['id'])) {
     $ID = $db->escapeString($fn->xss_clean($_GET['id']));
 } else {
-    // Handle the case when 'id' is not set or invalid.
-    // For example, you can redirect the user to an error page or display an error message.
-    echo "Invalid ID";
+    // $ID = "";
+    return false;
     exit(0);
 }
 
@@ -21,8 +19,9 @@ if (isset($_POST['btnUpdate'])) {
     $description = $db->escapeString($_POST['description']);
 
     if (!empty($title) && !empty($description)) {
-        $sql_query = "UPDATE balli_sasthram SET title='$title', description='$description' WHERE id = $ID";
+        $sql_query = "UPDATE balli_sasthram SET title='$title',description='$description' WHERE id =$ID";
         $db->sql($sql_query);
+        $res = $db->getResult();
         $update_result = $db->getResult();
 
         if (!empty($update_result)) {
@@ -45,40 +44,41 @@ if (isset($_POST['btnUpdate'])) {
                 }
             }
 
-            if (isset($_POST['insert_sub_description1']) && isset($_POST['insert_sub_description2'])) {
+            if (
+                isset($_POST['insert_sub_description1']) && isset($_POST['insert_sub_description2'])
+            ) {
                 for ($i = 0; $i < count($_POST['insert_sub_description1']); $i++) {
-                    $sub_description1 = $db->escapeString($_POST['insert_sub_description1'][$i]);
-                    $sub_description2 = $db->escapeString($_POST['insert_sub_description2'][$i]);
+                    $sub_description1 = $db->escapeString(($_POST['insert_sub_description1'][$i]));
+                    $sub_description2 = $db->escapeString(($_POST['insert_sub_description2'][$i]));
                     if (!empty($sub_description1) || !empty($sub_description2)) {
-                        $sql = "INSERT INTO balli_sasthram_variant (balli_sasthram_id, sub_description1, sub_description2) VALUES ('$ID', '$sub_description1', '$sub_description2')";
+                        $sql = "INSERT INTO balli_sasthram_variant (balli_sasthram_id,sub_title1,sub_title2,sub_description1,sub_description2) VALUES('$ID','$sub_title1','$sub_title2','$sub_description1','$sub_description2')";
                         $db->sql($sql);
                     }
                 }
             }
 
-            $error['update_ballisasthram'] = "<section class='content-header'><span class='label label-success'>Billi sasthram Tab updated Successfully</span></section>";
+            $error['update_ballisasthram'] = " <section class='content-header'><span class='label label-success'>Billi sasthram Tab updated Successfully</span></section>";
         } else {
-            $error['update_ballisasthram'] = "<span class='label label-danger'>Failed to update</span>";
+            $error['update_ballisasthram'] = " <span class='label label-danger'>Failed to update</span>";
         }
     }
 }
 
 // Get the existing data from the database
 $data = array();
-$sql_query = "SELECT * FROM balli_sasthram WHERE id = $ID";
+$sql_query = "SELECT * FROM balli_sasthram WHERE id =" . $ID;
 $db->sql($sql_query);
 $res = $db->getResult();
 
-$sql_query = "SELECT * FROM balli_sasthram_variant WHERE balli_sasthram_id = $ID";
+$sql_query = "SELECT * FROM balli_sasthram_variant WHERE balli_sasthram_id =" . $ID;
 $db->sql($sql_query);
 $resslot = $db->getResult();
 
-if (isset($_POST['btnCancel'])) {
-    // Redirect the user to the desired page upon cancellation.
-    header("Location: ballisasthram.php");
-    exit();
-}
-?>
+if (isset($_POST['btnCancel'])) { ?>
+    <script>
+        window.location.href = "ballisasthram.php";
+    </script>
+<?php } ?>
 <section class="content-header">
     <h1>Edit Balli Sasthram <small><a href='ballisasthram.php'> <i class='fa fa-angle-double-left'></i>&nbsp;&nbsp;&nbsp;Back to Balli Sasthram </a></small></h1>
     <?php echo isset($error['update_ballisasthram']) ? $error['update_ballisasthram'] : ''; ?>
@@ -106,7 +106,7 @@ if (isset($_POST['btnCancel'])) {
                                     <label for="exampleInputEmail1">Description</label> <i class="text-danger asterik">*</i>
                                     <textarea type="text" rows="3" class="form-control" name="description"><?php echo $res[0]['description'] ?></textarea>
                                 </div>
-</div>
+                            </div>
                         </div>
                         <br>
                         <div id="variations">
@@ -117,43 +117,28 @@ if (isset($_POST['btnCancel'])) {
                                 <div id="packate_div">
                                     <div class="row">
                                         <input type="hidden" class="form-control" name="balli_sasthram_variant_id[]" id="balli_sasthram_id" value='<?= $row['id']; ?>' />
-                                        <div class="col-md-4">
+                                        <div class="col-md-5">
                                             <div class="form-group packate_div">
-                                                <label for="exampleInputEmail1">Sub Tilte1</label> <i class="text-danger asterik">*</i>
-                                                <textarea type="text" rows="2" class="form-control" name="sub_title1[]"><?php echo $row['sub_title1'] ?></textarea>
+                                                <label for="exampleInputEmail1">Sub title1</label> <i class="text-danger asterik">*</i>
+                                                <input type="text" class="form-control" name="sub_title1[]" value="<?php echo $row['sub_title1'] ?>" />
                                             </div>
                                         </div>
-                                        <div class="col-md-4">
+                                        <div class="col-md-5">
                                             <div class="form-group packate_div">
-                                                <label for="exampleInputEmail1">Sub Title2</label> <i class="text-danger asterik">*</i>
-                                                <textarea type="text" rows="2" class="form-control" name="sub_title2[]"><?php echo $row['sub_title2'] ?></textarea>
+                                                <label for="exampleInputEmail1">Sub title2</label> <i class="text-danger asterik">*</i>
+                                                <input type="text" class="form-control" name="sub_title2[]" value="<?php echo $row['sub_title2'] ?>" />
                                             </div>
                                         </div>
-                                     
-                                    </div>
-                                </div>
-                            <?php $i++;
-                            } ?>
-                        </div>
-                    </div>
-                        <div id="variations">
-                            <?php
-                            $i = 0;
-                            foreach ($resslot as $row) {
-                                ?>
-                                <div id="packate_div">
-                                    <div class="row">
-                                        <input type="hidden" class="form-control" name="balli_sasthram_variant_id[]" id="balli_sasthram_id" value='<?= $row['id']; ?>' />
-                                        <div class="col-md-4">
+                                        <div class="col-md-5">
                                             <div class="form-group packate_div">
                                                 <label for="exampleInputEmail1">Sub description1</label> <i class="text-danger asterik">*</i>
-                                                <textarea type="text" rows="2" class="form-control" name="sub_description1[]"><?php echo $row['sub_description1'] ?></textarea>
+                                                <textarea type="text" rows="3" class="form-control" name="sub_description1[]"><?php echo $row['sub_description1'] ?></textarea>
                                             </div>
                                         </div>
-                                        <div class="col-md-4">
+                                        <div class="col-md-5">
                                             <div class="form-group packate_div">
                                                 <label for="exampleInputEmail1">Sub description2</label> <i class="text-danger asterik">*</i>
-                                                <textarea type="text" rows="2" class="form-control" name="sub_description2[]"><?php echo $row['sub_description2'] ?></textarea>
+                                                <textarea type="text" rows="3" class="form-control" name="sub_description2[]"><?php echo $row['sub_description2'] ?></textarea>
                                             </div>
                                         </div>
                                         <?php if ($i == 0) { ?>
@@ -169,7 +154,8 @@ if (isset($_POST['btnCancel'])) {
                                                 </a>
                                             </div>
                                         <?php } ?>
-                                    
+                                    </div>
+                                </div>
                             <?php $i++;
                             } ?>
                         </div>
