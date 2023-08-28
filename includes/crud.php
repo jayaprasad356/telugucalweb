c7b8c3c8a211e1a8d7b756c7fd026089cd8df0d7
AKraja777 <ashokkumar7hitter@gmail.com>
2023-08-28 19:43:01 +0530

    crud.php

--- includes/crud.php
+++ includes/crud.php
@@ -1,7 +1,7 @@
 <?php
 
 
-define('DOMAIN_URL', 'https://admin.vibametelugucalendar.online/'); /* chnage to your domain here - don't forget to add forward slash at the end of the URL like this "/" */
+define('DOMAIN_URL', 'https://admin.vibametelugucalendar.in/'); /* chnage to your domain here - don't forget to add forward slash at the end of the URL like this "/" */
 define('ALLOW_MODIFICATION', 1);
 define('JWT_SECRET_KEY', '12345678'); /* chnage to your secret key here set it to anything you like*/
 
